e7f15b9b52bc08e47a27aa8dc977ea6c7aa1392f
Brian Jacobel <bjacobel@gmail.com>
2014-08-10 23:04:17 -0400

    Grabbing the first 30-ish images and upsizing them

--- main.go
+++ main.go
@@ -12,15 +12,19 @@ func main() {
 	var e error
 
 	search := "inspirational quotes"
-	r, _ := regexp.Compile(" ")
-	search = string(r.ReplaceAll([]byte(search), []byte("%20"))) // lol golang's regexp library is fucked
+	spaces, _ := regexp.Compile(" ")
+	search = string(spaces.ReplaceAll([]byte(search), []byte("%20"))) // lol golang's regexp library is fucked
 
 	if doc, e = goquery.NewDocument("http://www.pinterest.com/search/pins/?q=" + search); e != nil {
 		log.Fatal(e)
 	}
 
+	urls, _ := regexp.Compile("(http(s)?://)?([\\w-]+\\.)+[\\w-]+(/[\\w- ;,./?%&=]*)?")
+	resizer, _ := regexp.Compile("236")
+
 	doc.Find(".fadeContainer").Each(func(i int, s *goquery.Selection) {
 		html, _ := (*s).Html()
-		fmt.Println(html)
+		url := string(resizer.ReplaceAll(urls.Find([]byte(html)), []byte("736")))
+		fmt.Println(url)
 	})
 }
