f6522f95b4e68a795f8394779a858136b2852e57
Brian Jacobel <bjacobel@gmail.com>
2014-08-12 23:19:17 -0400

    Downloading images with goroutines

--- main.go
+++ main.go
@@ -3,32 +3,67 @@ package main
 import (
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
+	"github.com/dchest/uniuri"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"regexp"
+	"sync"
 )
 
 var urls, _ = regexp.Compile("(http(s)?://)?([\\w-]+\\.)+[\\w-]+(/[\\w- ;,./?%&=]*)?")
 var resizer, _ = regexp.Compile("236")
 
 func main() {
-	var doc *goquery.Document
-	var e error
+	var wg sync.WaitGroup
 
 	search := "inspirational quotes"
 	spaces, _ := regexp.Compile(" ")
 	search = string(spaces.ReplaceAll([]byte(search), []byte("%20"))) // lol golang's regexp library is fucked
 
-	if doc, e = goquery.NewDocument("http://www.pinterest.com/search/pins/?q=" + search); e != nil {
+	if doc, e := goquery.NewDocument("http://www.pinterest.com/search/pins/?q=" + search); e != nil {
 		log.Fatal(e)
+	} else {
+		doc.Find(".fadeContainer").Each(func(i int, s *goquery.Selection) {
+			wg.Add(1)
+			go getImgFromNode(s, &wg)
+		})
 	}
 
-	doc.Find(".fadeContainer").Each(func(i int, s *goquery.Selection) {
-		go getImgSrcFromNode(s)
-		fmt.Println()
-	})
+	wg.Wait()
 }
 
-func getImgSrcFromNode(s *goquery.Selection) string {
-	html, _ := (*s).Html()
-	return string(resizer.ReplaceAll(urls.Find([]byte(html)), []byte("736")))
+func getImgFromNode(s *goquery.Selection, wg *sync.WaitGroup) {
+	html, err := (*s).Html()
+	handleErr(err)
+
+	imgLoc := string(resizer.ReplaceAll(urls.Find([]byte(html)), []byte("736")))
+
+	fmt.Println("Downloading " + imgLoc)
+
+	imgData, err := http.Get(imgLoc)
+	handleErr(err)
+
+	defer func() {
+		err := imgData.Body.Close()
+		handleErr(err)
+	}()
+
+	body, err := ioutil.ReadAll(imgData.Body)
+	handleErr(err)
+
+	path := "images/" + uniuri.New() + ".jpg"
+
+	fmt.Println("Saving to " + path)
+
+	err = ioutil.WriteFile(path, body, 0644)
+	handleErr(err)
+
+	wg.Done()
+}
+
+func handleErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
--- readme.md
+++ readme.md
@@ -2,3 +2,8 @@
 ---
 
 Speedy OCR of weird shit people put on Pinterest.
+
+Requirements:
+    - [tesseract-ocr](https://code.google.com/p/tesseract-ocr/)
+    - [imagemagick](http://www.imagemagick.org/)
+    - go 1.3
