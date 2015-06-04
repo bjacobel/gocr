811c2baa2aad1e18fcc352e3549bc8dba06fc997
Brian Jacobel <brian@bjacobel.com>
2015-06-04 14:08:07 -0400

    Updated to run with and without goroutines, and to print speed differences

--- main.go
+++ main.go
@@ -10,34 +10,67 @@ import (
 	"net/http"
 	"regexp"
 	"sync"
+	"time"
 )
 
-var urls, _ = regexp.Compile("(http(s)?://)?([\\w-]+\\.)+[\\w-]+(/[\\w- ;,./?%&=]*)?")
+var urls, _ = regexp.Compile("(http(s)?://)([\\w-]+\\.)+[\\w-]+(/[\\w- ;,./?%&=]*)?")
 var resizer, _ = regexp.Compile("236")
 
 func main() {
-	var wg sync.WaitGroup
 
 	search := "inspirational quotes"
 	spaces, _ := regexp.Compile(" ")
 	search = string(spaces.ReplaceAll([]byte(search), []byte("%20"))) // lol golang's regexp library is fucked
 
-	if doc, e := goquery.NewDocument("http://www.pinterest.com/search/pins/?q=" + search); e != nil {
+	if doc, e := goquery.NewDocument("https://www.pinterest.com/search/pins/?q=" + search); e != nil {
 		log.Fatal(e)
 	} else {
 
 		client, _ := gosseract.NewClient()
+		var syncStart time.Time
+		var asyncStart time.Time
+		var syncDeltas float64
+		var asyncDeltas float64
+		iters := 10
 
-		doc.Find(".fadeContainer").Each(func(i int, s *goquery.Selection) {
-			wg.Add(1)
-			go OcrFromNode(s, &wg, client)
-		})
+		// do image retrieval and OCR 100 times without goroutines, avg the runtime
+		for i := 1; i <= iters; i++ {
+			syncStart = time.Now()
+
+			doc.Find(".fadeContainer").Each(func(i int, s *goquery.Selection) {
+				OcrFromNode(s, client)
+			})
+
+			syncDeltas += time.Since(syncStart).Seconds()
+
+			fmt.Print("\rCompleted synchronous iteration ", i)
+		}
+
+		fmt.Println("... done. Synchronous execution completed in avg ", syncDeltas/float64(iters), "s")
+
+		// now do it with goroutines
+		for i := 1; i <= iters; i++ {
+			asyncStart = time.Now()
+
+			var wg sync.WaitGroup
+
+			doc.Find(".fadeContainer").Each(func(i int, s *goquery.Selection) {
+				wg.Add(1)
+				go OcrFromNode(s, client, &wg)
+			})
+
+			wg.Wait()
+
+			asyncDeltas += time.Since(asyncStart).Seconds()
+
+			fmt.Print("\rCompleted goroutine iteration ", i)
+		}
+
+		fmt.Println("... done. Goroutine execution completed in avg ", asyncDeltas/float64(iters), "s")
 	}
-
-	wg.Wait()
 }
 
-func OcrFromNode(s *goquery.Selection, wg *sync.WaitGroup, client *gosseract.Client) {
+func OcrFromNode(s *goquery.Selection, client *gosseract.Client, wg ...*sync.WaitGroup) {
 	html, err := (*s).Html()
 	handleErr(err)
 
@@ -59,12 +92,14 @@ func OcrFromNode(s *goquery.Selection, wg *sync.WaitGroup, client *gosseract.Cli
 	err = ioutil.WriteFile(path, body, 0644)
 	handleErr(err)
 
-	out, _ := client.Src(path).Out()
+	client.Src(path).Out()
 
-	fmt.Println("---------------\nText OCRed from " + imgLoc + ":\n")
-	fmt.Println(out + "\n")
+	// fmt.Println("---------------\nText OCRed from " + imgLoc + ":\n")
+	// fmt.Println(out + "\n")
 
-	wg.Done()
+	if wg != nil {
+		wg[0].Done()
+	}
 }
 
 func handleErr(err error) {
